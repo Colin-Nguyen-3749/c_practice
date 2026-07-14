c32c1ea974f340c310c3f120379d8c1f620d9bfc
Colin Nguyen <nguyen_c9@denison.edu>
2026-07-14 15:30:59 -0400

    Use ternerary operators

--- hello.c
+++ hello.c
@@ -13,6 +13,19 @@ int main(void) {
         printf("x is true!\n");
     }
 
+    // ternary operator
+    // if x > 10, add 17 to y. Otherwise add 37 to y
+    int y = 0;
+    int q = 4;
+    y += q > 10? 17: 37;
+
+    // Another example of a ternary operator
+    // %d refers to x, and %s refers to the string at the end
+    // If x % 2 is 0, then print 'even', otherwise print 'odd'
+    printf("The number %d is %s.\n", x, x % 2 == 0? "even": "odd");
+
+    // printf("This is y if q is " + q + ": " + y);
+
     // printf("Hello, World!\n");
     printf("%s i = %d and f = %f!\n", s, i, f);
 }
