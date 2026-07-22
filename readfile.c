203cdf02f80adbf1f4091bdf3f47e60584c750da
Colin Nguyen <nguyen_c9@denison.edu>
2026-07-22 17:32:48 -0400

    Read out all characters

--- readfile.c
+++ readfile.c
@@ -5,15 +5,17 @@ int main(void) {
     // This is a variable to represent open file
     FILE *fp;
 
+    int c;
+
     // Open file for reading
     // "r" means to open a text stream for reading
     fp = fopen("hello.txt", "r");
 
-    // Read a single character
-    int c = fgetc(fp);
-
-    // Print char to stdout
-    printf("%c\n", c);
+    // EOF = End of file
+    while ((c = fgetc(fp)) != EOF) {
+        // Print char to stdout
+        printf("%c", c);
+    }
 
     // Close the file when done
     fclose(fp);
